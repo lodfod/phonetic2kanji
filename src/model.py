ec60026067b73a3944f380d323d7c18be9c2ce37
archish-edu <archisharun99@gmail.com>
2025-03-04 12:20:27 -0800

    gitignore

--- src/model.py
+++
@@ -1,6 +0,0 @@
-from transformers import EncoderDecoderModel, BertTokenizer
-
-# Load a pre-trained model as a starting point
-model = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-multilingual-cased', 
-                                                              'bert-base-multilingual-cased')
-tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
